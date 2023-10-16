fb1a9385cafc5e66c813dadec5be72c147d834d1
maeng0830 <kmk930830@gmail.com>
2023-10-16 22:19:23 +0900

    Feat: app.v1~v2 - 프록시팩토리를 통해 동적 프록시 생성 - 구현   - 프록시팩토리를 통해 인터페이스가 있는 구현 클래스, 인터페이스가 없는 구체 클래스에 대해 일관적인 방법으로 동적 프록시를 생성할 수 있었다.   - 어드바이저를 통해 동적 프록시의 실행 로직을 일관적인 방법으로 적용할 수 있었다. - 문제   - 설정 파일이 지나치게 많아지게 된다. 예를 들어 스프링 빈으로 등록될 클래스가 100개가 있고, 해당 빈들에게 프록시 기능을 적용하려면 각 빈들에 대한 동적 프록시 생성 코드를 작성해줘야한다.   - 컴포넌트 스캔으로 빈을 등록할 경우, 이 방법은 사용이 불가능하다. 컴포넌트 스캔을 통해 실제 객체가 빈으로 등록되기 때문이다.

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -3,6 +3,8 @@ package hello.proxy;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -15,14 +17,14 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+@Import({ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
-
 
 	@Bean
 	public LogTrace logTrace() {
